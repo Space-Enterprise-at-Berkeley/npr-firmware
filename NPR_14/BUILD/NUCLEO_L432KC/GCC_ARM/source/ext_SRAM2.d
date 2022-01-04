19059dc877230a04dcf703be71ec95af1928a590
Justin Yang <justin.yang777@gmail.com>
2022-01-04 15:13:57 -0800

    remove old fw

--- NPR_14/BUILD/NUCLEO_L432KC/GCC_ARM/source/ext_SRAM2.d
+++
@@ -1,2 +0,0 @@
-BUILD\NUCLEO_L432KC\GCC_ARM\source\ext_SRAM2.o: source\ext_SRAM2.cpp \
- BUILD\NUCLEO_L432KC\GCC_ARM\mbed_config.h
--- radio_config_generation_2/SI4463_which_config.h
+++
@@ -1,3 +0,0 @@
-#define which_modulation 24
-#define which_band "_2m"
-#include "./SI4463_2m/radio_config_Si4463_24.h"
